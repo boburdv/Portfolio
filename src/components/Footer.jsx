bb95f2e8b19113d1ee668641ad9ed3c855546703
boburdv <boburdv@gmail.com>
2026-04-15 02:39:44 -0700

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,5 @@ import React from "react";
 
 export default function Footer() {
   const year = new Date().getFullYear();
-
   return <footer className="py-6 tracking-wide text-center text-gray-500">© {year} boburdv.uz</footer>;
 }
